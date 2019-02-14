ed24c79395f3c2e9239c211a3e9ac189c77df42a
Jake Howard <git@theorangeone.net>
2019-02-14 20:36:48 +0000

    add todoist to healthcheck

--- actioner/web/healthcheck.py
+++ actioner/web/healthcheck.py
@@ -1,9 +1,11 @@
 from aiohttp import web
 
-from actioner.clients import github
+from actioner.clients import github, todoist
 
 
 async def healthcheck(request):
+    todoist.user.sync()
     return web.json_response({
-        'github': github.get_user().login
+        'github': github.get_user().login,
+        'todoist': todoist.user.get_id()
     })
