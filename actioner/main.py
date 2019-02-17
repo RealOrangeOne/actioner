a76a1299d39cf0e1c9474c51a8b8407364bd6699
Jake Howard <git@theorangeone.net>
2019-02-17 17:14:17 +0000

    Add sentry integration

--- actioner/main.py
+++ actioner/main.py
@@ -1,15 +1,20 @@
 import logging
 from multiprocessing import Process
 
+import sentry_sdk
 from aiohttp.web import run_app as run_web_app
 
 from actioner.scheduler import create_scheduler, start_scheduler
-from actioner.settings import LOGGING_LEVEL
+from actioner.settings import LOGGING_LEVEL, SENTRY_DSN
 from actioner.web import get_server
 
 
 def main():
     logging.basicConfig(level=LOGGING_LEVEL)
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+    )
 
     server = get_server()
     scheduler = create_scheduler()
--- actioner/settings.py
+++ actioner/settings.py
@@ -5,6 +5,8 @@ GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
 
 TODOIST_TOKEN = os.environ['TODOIST_TOKEN']
 
+SENTRY_DSN = os.environ.get('SENTRY_DSN', '')
+
 LOGGING_LEVEL = _nameToLevel[os.environ.get('LOGGING_LEVEL', 'INFO')]
 
 BASIC_AUTH = os.environ['BASIC_AUTH'].split(":")
