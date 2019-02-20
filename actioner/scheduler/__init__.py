2e1628641caf04767dece51cce213c803bb503b8
Jake Howard <git@theorangeone.net>
2019-02-20 20:00:44 +0000

    Force interval jobs to run on startup

--- actioner/scheduler/__init__.py
+++ actioner/scheduler/__init__.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 
 from .todoist_assigned_issues import todoist_assigned_issues
 from .todoist_repo_prs import todoist_repo_prs
@@ -10,6 +11,11 @@ def create_scheduler():
     scheduler = AsyncIOScheduler()
     scheduler.add_job(todoist_assigned_issues, 'interval', minutes=15)
     scheduler.add_job(todoist_repo_prs, 'interval', minutes=15)
+
+    for job in scheduler.get_jobs():
+        if isinstance(job.trigger, IntervalTrigger):
+            scheduler.add_job(job.func)
+
     return scheduler
 
 
