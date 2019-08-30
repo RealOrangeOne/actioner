1f8dbf310df9ae82182e4ef51c8dd37402e917c9
Jake Howard <git@theorangeone.net>
2019-08-30 21:45:28 +0100

    Remove web tests, for now

--- tests/test_web/__init__.py
+++
@@ -1,8 +0,0 @@
-from aiohttp.test_utils import AioHTTPTestCase
-
-from actioner.web import get_server
-
-
-class BaseWebTestCase(AioHTTPTestCase):
-    async def get_application(self):
-        return get_server()
