2ef14e8d9e290c40935ca3cf2a9be2714373dfbc
Jake Howard <git@theorangeone.net>
2019-08-30 22:20:32 +0100

    Allow running scheduled jobs once

--- actioner/main.py
+++ actioner/main.py
@@ -1,12 +1,22 @@
+import argparse
 import logging
 from multiprocessing import Process
 
 import sentry_sdk
+from apscheduler.util import get_callable_name
 from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 
 from actioner.scheduler import create_scheduler
 from actioner.settings import LOGGING_LEVEL, SENTRY_DSN
 from actioner.web import get_server, run_server
+
+logger = logging.getLogger(__name__)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--once", action="store_true")
+    return parser.parse_args()
 
 
 def main():
@@ -14,12 +24,17 @@ def main():
 
     sentry_sdk.init(dsn=SENTRY_DSN, integrations=[AioHttpIntegration()])
 
-    server = get_server()
-    scheduler = create_scheduler()
+    args = parse_args()
 
-    # HACK: APScheduler doesn't like running in an external event loop. https://github.com/agronholm/apscheduler/issues/360
-    Process(target=run_server, args=(server,)).start()
-    Process(target=scheduler.start).start()
+    if args.once:
+        scheduler = create_scheduler()
+        jobs = {job.func for job in scheduler.get_jobs()}
+        for job in jobs:
+            logger.info("Executing '{}'".format(get_callable_name(job)))
+            job()
+    else:
+        Process(target=run_server, args=(get_server(),)).start()
+        Process(target=create_scheduler().start).start()
 
 
 if __name__ == "__main__":
